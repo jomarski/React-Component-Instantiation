e72c512fb93fd69253698838016df564e0911e25
jomarski <116093622+jomarski@users.noreply.github.com>
2022-10-27 20:10:35 +0800

    Add files via upload

---
+++ React Component Instantiation/App1.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+   
+      <Search />
+    
+      <hr />
+     
+      {/* creating an instance of List component */}
+      <List />
+      {/* creating another instance of List component */}
+      <List />
+    </div>
+  );
+}
